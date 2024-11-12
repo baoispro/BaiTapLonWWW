848d7381f53ca8f9535253f4aa8e59392488015e
baoispro <nguyenlegiabao810@gmail.com>
2024-11-12 20:50:20 +0700

    complete models

---
+++ src/main/java/vn/edu/iuh/fit/backend/models/CandidateSkill.java
@@ -0,0 +1,32 @@
+package vn.edu.iuh.fit.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import vn.edu.iuh.fit.backend.ids.CandidateSkillId;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "candidate_skill")
+public class CandidateSkill {
+    @EmbeddedId
+    private CandidateSkillId id;
+
+    @MapsId("canId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "can_id", nullable = false)
+    private vn.edu.iuh.fit.Candidate can;
+
+    @MapsId("skillId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "skill_id", nullable = false)
+    private vn.edu.iuh.fit.Skill skill;
+
+    @Column(name = "more_infos", length = 1000)
+    private String moreInfos;
+
+    @Column(name = "skill_level", nullable = false)
+    private Byte skillLevel;
+
+}
---
+++ src/main/java/vn/edu/iuh/fit/backend/models/JobSkill.java
@@ -0,0 +1,32 @@
+package vn.edu.iuh.fit.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import vn.edu.iuh.fit.backend.ids.JobSkillId;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "job_skill")
+public class JobSkill {
+    @EmbeddedId
+    private JobSkillId id;
+
+    @MapsId("jobId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "job_id", nullable = false)
+    private vn.edu.iuh.fit.Job job;
+
+    @MapsId("skillId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "skill_id", nullable = false)
+    private vn.edu.iuh.fit.Skill skill;
+
+    @Column(name = "more_infos", length = 1000)
+    private String moreInfos;
+
+    @Column(name = "skill_level", nullable = false)
+    private Byte skillLevel;
+
+}
