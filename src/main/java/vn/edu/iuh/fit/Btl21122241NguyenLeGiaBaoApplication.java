2818efaf817a48b8da3211f188594fa95bc2d95f
baoispro <nguyenlegiabao810@gmail.com>
2024-11-12 19:17:26 +0700

    Initial commit

---
+++ src/main/java/vn/edu/iuh/fit/Btl21122241NguyenLeGiaBaoApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Btl21122241NguyenLeGiaBaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Btl21122241NguyenLeGiaBaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/Btl21122241NguyenLeGiaBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Btl21122241NguyenLeGiaBaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
